aafe7529010f36251a48006732d3a20d77cd7af6
softgenai <admin@softgen.ai>
2025-01-08 01:21:14 +0000

    wip

--- init-workspace.sh
+++ init-workspace.sh
@@ -6,8 +6,6 @@ if [ ! -f "/app/package.json" ]; then
     cd /template-files && find . -type f ! -name 'init-workspace.sh' ! -name 'Dockerfile' -exec cp --parents {} /app/ \;
     cd /template-files && find . -type f -name '.*' ! -name 'init-workspace.sh' ! -name 'Dockerfile' -exec cp --parents {} /app/ \; 2>/dev/null || true
     
-    echo "Installing dependencies..."
-    cd /app && npm install
 else
     echo "Workspace already initialized, skipping setup..."
 fi
